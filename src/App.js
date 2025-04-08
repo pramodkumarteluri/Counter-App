afd6ba99c6448493c1fa60f0bcf4760cdbfc8b29
pramodkumarteluri <pramodkumarteluri@gmail.com>
2025-04-08 22:57:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+const [count, setCount] = useState(0);
+
+const getCountColorClass = () => {
+  if (count > 0) return 'green';
+  if (count < 0) return 'red';
+  return 'blue'
+}
+
+const handleIncrement = () => {
+  setCount(count + 1);
+}
+
+const handleDecrement = () => {
+  setCount(count - 1);
+}
+
+const handleReset = () => {
+  setCount(0);
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Counter App</h1>
+
+      {/* Displays the current count */}
+      <h2 className={`count-display ${getCountColorClass()}`}>{count}</h2>
+      {/* Buttons To Control the counter */}
+        <div className="button-grp">
+          <button onClick={handleDecrement}>-</button>
+          <button onClick={handleReset}>Reset</button>
+          <button onClick={handleIncrement}>+</button>
+        </div>
     </div>
   );
 }
